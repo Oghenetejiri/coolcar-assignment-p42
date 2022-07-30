36456e6b11805ff2e27482689f168ba58e063409
Oghenetejiri Frances Otor <78227200+Oghenetejiri@users.noreply.github.com>
2022-07-30 20:55:56 +0300

    Cleaned code

--- coolcar.js
+++ coolcar.js
@@ -2,17 +2,13 @@ let now = new Date();
 
 let h2 = document.querySelector("h2");
 
-
 let year = now.getFullYear();
 let month = now.getMonth();
 let date = now.getDate(); 
 
-
 let hours = now.getHours();
 let minutes = now.getMinutes(); 
 let seconds = now.getSeconds()
-
-
 
 function search(event){
 
